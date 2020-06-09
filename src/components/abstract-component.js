99dead8433b4ae5315b7f21d744e2835becbd715
Dmitry Kuchukov <kudim84@yandex.ru>
2020-06-10 01:37:03 +0300

    Вынесет в абстрактный класс общие методы

--- src/components/abstract-component.js
+++ src/components/abstract-component.js
@@ -3,5 +3,19 @@ export default class AbstractComponent {
     if (new.target === AbstractComponent) {
       throw new Error(`Can't instantiate AbstractComponent, only concrete one.`);
     }
+
+    this._element = null;
+  }
+
+  getElement() {
+    if (!this._element) {
+      this._element = createElement(this.getTemplate());
+    }
+
+    return this._element;
+  }
+
+  removeElement() {
+    this._element = null;
   }
 }
