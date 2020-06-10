7333e4dd1f648ef766ca35f4d9285240e988adc6
kuzmich84 <kudim@inbox.ru>
2020-06-10 19:01:17 +0300

    Перенесет логику отрисовки карточки в контроллер

--- src/controllers/board.js
+++ src/controllers/board.js
@@ -1,3 +1,46 @@
+import CardFilmComponent from "../components/card";
+import DetailsFilmComponent from "../components/details";
+import {remove, render, RenderPosition} from "../utils/render";
+
+const renderCard = (card, container) => {
+  const cardComponent = new CardFilmComponent(card);
+  const detailsFilmComponent = new DetailsFilmComponent(card);
+
+  const onEscKeyDown = (evt) => {
+    const isEscKey = evt.key === `Escape` || evt.key === `Esc`;
+
+    if (isEscKey) {
+      remove(detailsFilmComponent);
+      document.removeEventListener(`keydown`, onEscKeyDown);
+    }
+  };
+
+  const showDetails = () => {
+    render(document.querySelector(`body`), detailsFilmComponent, RenderPosition.BEFOREEND);
+    document.addEventListener(`keydown`, onEscKeyDown);
+  };
+
+  cardComponent.setShowDetailsClickHandler(() => {
+    showDetails();
+  }, `.film-card__poster`);
+
+  cardComponent.setShowDetailsClickHandler(() => {
+    showDetails();
+  }, `.film-card__title`);
+
+  cardComponent.setShowDetailsClickHandler(() => {
+    showDetails();
+  }, `.film-card__comments`);
+
+  const detailsButton = detailsFilmComponent.getElement().querySelector(`.film-details__close-btn`);
+  detailsButton.addEventListener(`click`, () => {
+    remove(detailsFilmComponent);
+  });
+
+  render(container, cardComponent, RenderPosition.BEFOREEND);
+
+};
+
 export default class BoardController {
 
 }
--- src/main.js
+++ src/main.js
@@ -21,44 +21,6 @@ const cards = generateCards(FILM_COUNT).map((item, index) => {
   return item;
 });
 
-const renderCard = (card, container) => {
-  const cardComponent = new CardFilmComponent(card);
-  const detailsFilmComponent = new DetailsFilmComponent(card);
-
-  const onEscKeyDown = (evt) => {
-    const isEscKey = evt.key === `Escape` || evt.key === `Esc`;
-
-    if (isEscKey) {
-      remove(detailsFilmComponent);
-      document.removeEventListener(`keydown`, onEscKeyDown);
-    }
-  };
-
-  const showDetails = () => {
-    render(document.querySelector(`body`), detailsFilmComponent, RenderPosition.BEFOREEND);
-    document.addEventListener(`keydown`, onEscKeyDown);
-  };
-
-  cardComponent.setShowDetailsClickHandler(() => {
-    showDetails();
-  }, `.film-card__poster`);
-
-  cardComponent.setShowDetailsClickHandler(() => {
-    showDetails();
-  }, `.film-card__title`);
-
-  cardComponent.setShowDetailsClickHandler(() => {
-    showDetails();
-  }, `.film-card__comments`);
-
-  const detailsButton = detailsFilmComponent.getElement().querySelector(`.film-details__close-btn`);
-  detailsButton.addEventListener(`click`, () => {
-    remove(detailsFilmComponent);
-  });
-
-  render(container, cardComponent, RenderPosition.BEFOREEND);
-
-};
 
 // формирует данные для статистики
 const statisticData = (arr) => {
